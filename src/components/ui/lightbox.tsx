72a4e57a47124b38c6558f6d4f4af775408f3af6
Luciano Frizzera <lucaju@me.com>
2026-01-14 19:59:26 -0500

    improve lightbox

--- src/components/ui/lightbox.tsx
+++ src/components/ui/lightbox.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react';
-import { Carousel, CarouselContent, CarouselItem } from './carousel';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from './dialog';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './carousel';
+import { Dialog, DialogContent } from './dialog';
 
 type LigthboxProps = {
 	images: Array<{ src: string; alt: string; title?: string }>;
@@ -21,27 +21,26 @@ export function Lightbox(props: LigthboxProps) {
 	return (
 		<>
 			<Dialog open={open} onOpenChange={setOpen}>
-				<DialogContent className="sm:max-w-2xl">
-					<DialogHeader>
-						<DialogTitle>Photo Details</DialogTitle>
-						{props.images.length > 1 && (
-							<DialogDescription>Swipe to scroll through the images</DialogDescription>
-						)}
-					</DialogHeader>
-
+				<DialogContent className="sm:max-w-4xl bg-transparent border-0 shadow-none p-8">
 					<Carousel key={index} opts={{ startIndex: index, active: true }}>
 						<CarouselContent>
 							{props.images.map((image) => (
-								<CarouselItem key={image.src} className="">
+								<CarouselItem key={image.src} className="flex justify-center">
 									<img
 										alt={image.alt}
-										className=" h-8- lg:h-144 lg:w-full object-center lg:object-scale-down object-contain"
+										className="lg:w-full rounded-sm object-center object-contain"
 										fetchPriority="low"
 										src={image.src}
 									/>
 								</CarouselItem>
 							))}
 						</CarouselContent>
+						{props.images.length > 1 && (
+							<>
+								<CarouselPrevious />
+								<CarouselNext />
+							</>
+						)}
 					</Carousel>
 				</DialogContent>
 			</Dialog>
